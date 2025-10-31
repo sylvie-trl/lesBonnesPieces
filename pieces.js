20fc34cad05c35e67e121cd3b1b5859b5afd38f8
Sylvie Trolle <sylvie.trolle@gustaveroussy.fr>
2025-10-31 14:03:55 +0100

    Refactor pieces.js to improve readability and add missing elements for description and availability

--- pieces.js
+++ pieces.js
@@ -1,19 +1,32 @@
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('pieces-autos.json');
+const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
-// Création des balises 
-const article = pieces[0];
+// Création des balises
+const article = pieces[2];
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
 const nomElement = document.createElement("h2");
 nomElement.innerText = article.nom;
 const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+prixElement.innerText = `Prix: ${article.prix} € (${
+  article.prix < 35 ? "€" : "€€€"
+})`;
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText =
+  article.description ?? "Pas de description pour le moment.";
+
+const disponibiliteElement = document.createElement("p");
+disponibiliteElement.innerText = article.disponibilite
+  ? "En stock"
+  : "Rupture de stock";
+
 //Rattachement de nos balises au DOM
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(disponibiliteElement);
